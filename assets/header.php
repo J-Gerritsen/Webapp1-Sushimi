d66e937113bf72644a69553ed13c28a9a6eb5bba
job-gerritsen <154206850+J-Gerritsen@users.noreply.github.com>
2024-04-12 21:58:36 +0200

    created the header

---
+++ assets/header.php
@@ -0,0 +1,12 @@
+<header class="header">
+        <a class="logo" href="../index.php">sushi<section class="imi">mi</section></a>
+        <nav class="main-mav">
+            <ul class="main-nav-list">
+                <li><a class="main-nav-link hover" href="../index.php">Home</a></li>
+                <li><a class="main-nav-link hover" href="../pages/order_page.php">Menu</a></li>
+                <li><a class="main-nav-link hover" href="../pages/book.php">Book</a></li>
+                <li><a class="main-nav-link hover" href="../pages/contact.php">Contact</a></li>
+                <li><a class="main-nav-link hover" href="../pages/login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
